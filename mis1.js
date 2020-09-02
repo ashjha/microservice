1fe4fd1c78ac769c0f9410036909f20b7e0c8de0
Ashutosh Jha <ashutoshjha@Ashutosh3150772.local>
2020-09-02 23:13:16 +0530

    first

---
+++ mis1.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// const hostname = '127.0.0.1';
+const port = 3000;
+const misNumber = 1;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`This is microservice ${misNumber} runnung on port ${port}`);
+});
+
+server.listen(port, () => {
+  //console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
+});
